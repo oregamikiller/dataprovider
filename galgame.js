aef906dd02ee95fc5fcc312ca8c3658f1dc94671
oregami <oregami@163.com>
2016-07-22 17:39:30 +0800

    fetch detail

--- galgame.js
+++ galgame.js
@@ -14,9 +14,9 @@ var Promise     = require('bluebird');
 var rp          = require('request-promise');
 var platforms   = ['ps4', 'ps3', 'psvita'];
 var tasks        = [];
-var tasks0       = [];
-var gamelist     = [];
 var target     = 'http://seiya-saiga.com/game/galge.html';
+var list = [];
+var detaillist = [];
 
 
 MongoClient.connect("mongodb://localhost:27017/gamepark", function (err, mdb) {
@@ -32,10 +32,43 @@ rp.get({
         'User-Agent': 'Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version'
     }
 }).then(function (res) {
-    console.log(iconv.decode(res, "shift_jis"));
-    //$            = cheerio.load(iconv.decode(res, "shift_jis"));
+    $ = cheerio.load(iconv.decode(res, "shift_jis"));
 
-    //$("tbody").find('tr').each(function (i, elem) {
-    //    console.log($(this).find('td').find('b').find('a').text());
-    //});
+    $("tbody").find('tr').each(function (i, elem) {
+       var title = $(this).find('td').eq(0).find('b').find('a').text();
+        if(title)
+        {var url = 'http://seiya-saiga.com/game/' + $(this).find('td').eq(0).find('b').find('a').attr('href');
+            var item = {title:title, url: url };
+            list.push(item);
+        }
+    });
+    return Promise.resolve('ok');
+}).then(function () {
+    list.forEach(function (item) {
+        tasks.push(rp.get({
+            url: item.url,
+            method: 'GET',
+            encoding: null,
+            headers: {
+                ContentType: 'application/x-www-form-urlencoded',
+                'User-Agent': 'Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version'
+            }
+        }));
+    });
+    return Promise.resolve('ok');
+}).then(function () {
+    Promise.all(tasks).then(function (results){
+        results.forEach(function (result) {
+                $ = cheerio.load(iconv.decode(result, "shift_jis"));
+                $("table").find('tbody').find('table').eq(2).find('tr').each(function (i, elem) {
+                    detaillist.push($(this).find('td').eq(0).text());
+                    console.log($(this).find('td').eq(0).text());
+
+                });
+            $("table").find('tbody').find('table').eq(2).find('tr').each(function (i, elem) {
+                detaillist.push($(this).find('td').eq(1).text());
+                console.log($(this).find('td').eq(1).text());
+            });
+        });
+    });
 });
